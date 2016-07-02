e3a955fca2f247437722e810ef1b1db0991a3830
KingNigel <693913967@qq.com>
2016-07-03 00:30:32 +0800

    添加use

--- bin/cli.js
+++ bin/cli.js
@@ -2,9 +2,15 @@
 var program = require('commander');
 var install=require('../lib/install');
 var list=require('../lib/list');
-var installDescription='The path must be the true path，If [templetName] is not set,the filename will be the default name.';
+var use=require('../lib/use');
+var installDescription='The path must be the true path，[alias] is a key to the templet .'+
+'\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'+
+'If [alias] is not set,the filename will be the default key.';
 var listDescription='List the templet.';
-var uninstallDescription='The alias must be the true alias，you can see it by \'-l\'.';
+var deleteDescription='The alias must be the true alias，you can see it by \'-l\'.';
+var useDescription='To initialize a file with a templet.<alias> is a key to a templet.'+
+'\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'+
+'A file well be created，[name] as a filename will be created.';
 
 // function range(val) {
 //   return val.split('..').map(Number);
@@ -26,9 +32,10 @@ var uninstallDescription='The alias must be the true alias，you can see it by \
 program
   .version('1.0.0')
   .usage('[options]')
-  .option('-i, --install <path>@[alias]',installDescription, install)
+  .option('-i, --install <path>@[alias]',installDescription,install)
   .option('-l, --list ',listDescription, list.showList)
-  .option('-u, --uninstall <alias>',uninstallDescription, list.del)
+  .option('-d, --delete <alias>',deleteDescription,list.del)
+  .option('-u, --use <alias>@[name]',useDescription,use)
   
 
 
