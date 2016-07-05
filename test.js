17785765048a3798503ad4778e7aaf49310cb2af
KingNigel <693913967@qq.com>
2016-07-05 22:01:41 +0800

    add delete

--- test.js
+++ test.js
@@ -3,10 +3,30 @@ const fs=require('fs');
 const exec = require('co-exec');
 
 test('install', function * (t) {
-    const commit = yield exec('node fi.js -i package.json@test');
-    t.true(fs.accessSync('./templet/test.json')===undefined);
+     const commit1 = yield exec('node fi.js -i package.json@test');
+     const commit2 = yield exec('node fi.js -i package.json');
+     t.true(fs.accessSync('./templet/test.json')===undefined);
+     t.true(fs.accessSync('./templet/package.json')===undefined);
+     const list=JSON.parse(fs.readFileSync('./list.json','utf8'));
+     t.true(list.test.fileName==='package.json'&&list.test.trueName==='test.json')
+     t.true(list.package.fileName==='package.json'&&list.package.trueName==='package.json')
 });
-test('install', function * (t) {
-    const commit = yield exec('node fi.js -i package.json@test');
-    t.true(fs.accessSync('./templet/test.json')===undefined);
+test('delete', function * (t) {
+    const commit1 = yield exec('node fi.js -i package.json@delete');
+     const list=JSON.parse(fs.readFileSync('./list.json','utf8'));
+     console.log(list);
+     t.true(fs.accessSync('./templet/delete.json')===undefined);
+     t.true(list.delete.fileName==='package.json'&&list.delete.trueName==='delete.json')     
+     const commit2 = yield exec('node fi.js -d delete');
+     t.false(false);
+     const err = new Error('The message');
+    
+     var a=function (params) {
+          throw err;
+     }
+     t.notThrows(a());
+    //  t.throws(fs.access('./templet/delete.json',(err)=>{
+    //       if(err) throw err;
+    //  }));
+     //t.true(list.delete===undefined);
 });
