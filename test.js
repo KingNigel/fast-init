5d5b14311fd8e805e992b5b69adfb6a39bae8e0d
KingNigel <693913967@qq.com>
2016-07-08 10:50:56 +0800

    '修改成同步测试'

--- test.js
+++ test.js
@@ -1,49 +1,43 @@
 import test from 'ava';
 const fs = require('fs');
-const exec = require('co-exec');
-
-test('install', function* (t) {
-    const command1 = yield exec('node fi.js -i package.json@test');
-    const command2 = yield exec('node fi.js -i package.json');
-    setTimeout(function () {
-        t.true(fs.accessSync('./templet/test.json') === undefined);
-        t.true(fs.accessSync('./templet/package.json') === undefined);
-        let list = JSON.parse(fs.readFileSync('./list.json', 'utf8'));
-        t.true(list.test.fileName === 'package.json' && list.test.trueName === 'test.json');
-        t.true(list.package.fileName === 'package.json' && list.package.trueName === 'package.json');
-    }, 1000);
+const exec = require('child_process').execSync;
+test.serial('install', (t) => {
+    exec('node fi.js -i package.json@test');
+    exec('node fi.js -i package.json');
+    t.true(fs.accessSync('./templet/test.json') === undefined);
+    t.true(fs.accessSync('./templet/package.json') === undefined);
+    let list = JSON.parse(fs.readFileSync('./list.json', 'utf8'));
+    t.true(list.test.fileName === 'package.json' && list.test.trueName === 'test.json');
+    t.true(list.package.fileName === 'package.json' && list.package.trueName === 'package.json');
 });
-test('delete', function* (t) {
-    const command2 = yield exec('node fi.js -d i');
-    setTimeout(function () {
-        fs.access('./templet/i.json', (err) => {
-            if (err) t.pass();
-            else t.fail();
-        });
-        list = JSON.parse(fs.readFileSync('./list.json', 'utf8'));
-        t.true(list.i === undefined);
-    }, 2000);
+test.serial('delete', (t) => {
+    exec('node fi.js -i package.json@delete');
+    exec('node fi.js -d delete');
+    fs.access('./templet/delete.json', (err) => {
+        if (err) t.pass();
+        else t.fail();
+    });
+    let list = JSON.parse(fs.readFileSync('./list.json', 'utf8'));
+    t.true(list.delete === undefined);
 });
-test('use', function* (t) {
-    const command1 = yield exec('node fi.js -u i');
-    const command2 = yield exec('node fi.js -u i@initAlias');
-    const command3 = yield exec('node fi.js -u i@initAlias.js');
-    setTimeout(function () {
-        fs.access('./init.json', (err) => {
-            if (err) t.fail();
-            else t.pass();
-        });
-        fs.access('./initAlias.json', (err) => {
-            if (err) t.fail();
-            else t.pass();
-        });
-        fs.access('./initAlias.js', (err) => {
-            if (err) t.fail();
-            else t.pass();
-        });
-    }, 3000);
+test.serial('use', (t) => {
+    exec('node fi.js -u i');
+    exec('node fi.js -u i@initAlias');
+    exec('node fi.js -u i@initAlias.js');
+    fs.access('./init.json', (err) => {
+        if (err) t.fail();
+        else t.pass();
+    });
+    fs.access('./initAlias.json', (err) => {
+        if (err) t.fail();
+        else t.pass();
+    });
+    fs.access('./initAlias.js', (err) => {
+        if (err) t.fail();
+        else t.pass();
+    });;
 });
-test('list', function* (t) {
-    const command = yield exec('node fi.js -l');
+test.serial('list', (t) => {
+    exec('node fi.js -l');
     t.pass();
 });
---
+++ test2.js
@@ -0,0 +1,49 @@
+import test from 'ava';
+const fs = require('fs');
+const exec = require('co-exec');
+
+test('install', function* (t) {
+    const command1 = yield exec('node fi.js -i package.json@test');
+    const command2 = yield exec('node fi.js -i package.json');
+    setTimeout(function () {
+        t.true(fs.accessSync('./templet/test.json') === undefined);
+        t.true(fs.accessSync('./templet/package.json') === undefined);
+        let list = JSON.parse(fs.readFileSync('./list.json', 'utf8'));
+        t.true(list.test.fileName === 'package.json' && list.test.trueName === 'test.json');
+        t.true(list.package.fileName === 'package.json' && list.package.trueName === 'package.json');
+    }, 1000);
+});
+test('delete', function* (t) {
+    const command2 = yield exec('node fi.js -d i');
+    setTimeout(function () {
+        fs.access('./templet/i.json', (err) => {
+            if (err) t.pass();
+            else t.fail();
+        });
+        list = JSON.parse(fs.readFileSync('./list.json', 'utf8'));
+        t.true(list.i === undefined);
+    }, 2000);
+});
+test('use', function* (t) {
+    const command1 = yield exec('node fi.js -u i');
+    const command2 = yield exec('node fi.js -u i@initAlias');
+    const command3 = yield exec('node fi.js -u i@initAlias.js');
+    setTimeout(function () {
+        fs.access('./init.json', (err) => {
+            if (err) t.fail();
+            else t.pass();
+        });
+        fs.access('./initAlias.json', (err) => {
+            if (err) t.fail();
+            else t.pass();
+        });
+        fs.access('./initAlias.js', (err) => {
+            if (err) t.fail();
+            else t.pass();
+        });
+    }, 3000);
+});
+test('list', function* (t) {
+    const command = yield exec('node fi.js -l');
+    t.pass();
+});
