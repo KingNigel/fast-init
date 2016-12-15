c2426c987ec5e522fd52b7d771a5231352fad7f6
KingNigel <693913967@qq.com>
2016-12-15 16:39:46 +0800

    修改提示信息

--- bin/cli.js
+++ bin/cli.js
@@ -1,16 +1,15 @@
-#!/usr/bin/env node
 var program = require('commander');
 var install=require('../lib/install');
 var list=require('../lib/list');
 var use=require('../lib/use');
-var installDescription='The path must be the true path，[alias] is a key to the templet .'+
-'\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'+
-'If [alias] is not set,the filename will be the default key.';
-var listDescription='List the templet.';
-var deleteDescription='The alias must be the true alias，you can see it by \'-l\'.';
-var useDescription='To initialize a file with a templet.<alias> is a key to a templet.'+
-'\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'+
-'A file well be created，[name] as a filename will be created.';
+ var installDescription='The path must be the true path，[alias] is a key to the templet.'+
+ '\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'+
+ 'If [alias] is not set,the filename will be the default key.';
+ var listDescription='List the templet.';
+ var deleteDescription='The alias must be the true alias，you can see it by \'-l\'.';
+ var useDescription='To initialize a file with a templet.<alias> is a key to a templet.'+
+ '\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'+
+ 'A file well be created，[name] as a filename will be created.';
 program
   .version('1.0.0')
   .usage('[options]')
