f80c7fe91eac7f593fa2500f743ef79a17806888
KingNigel <693913967@qq.com>
2018-11-30 15:21:49 +0800

    格式化

--- bin/cli.js
+++ bin/cli.js
@@ -1,21 +1,21 @@
-var program = require('commander');
-var install=require('../lib/install');
-var list=require('../lib/list');
-var use=require('../lib/use');
- var installDescription='The path must be the true path，[alias] is a key to the templet.'+
- '\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'+
- 'If [alias] is not set,the filename will be the default key.';
- var listDescription='List the templet.';
- var deleteDescription='The alias must be the true alias，you can see it by \'-l\'.';
- var useDescription='To initialize a file with a templet.<alias> is a key to a templet.'+
- '\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'+
- 'A file well be created，[name] as a filename will be created.';
+var program = require('commander')
+var install = require('../lib/install')
+var list = require('../lib/list')
+var use = require('../lib/use')
+var installDescription = 'The path must be the true path，[alias] is a key to the templet.' +
+ '\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0' +
+ 'If [alias] is not set,the filename will be the default key.'
+var listDescription = 'List the templet.'
+var deleteDescription = 'The alias must be the true alias，you can see it by \'-l\'.'
+var useDescription = 'To initialize a file with a templet.<alias> is a key to a templet.' +
+ '\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0' +
+ 'A file well be created，[name] as a filename will be created.'
 program
   .version('1.0.0')
   .usage('[options]')
-  .option('-i, --install <path>@[alias]',installDescription,install)
-  .option('-l, --list ',listDescription, list.showList)
-  .option('-d, --delete <alias>',deleteDescription,list.del)
-  .option('-u, --use <alias>@[name]',useDescription,use)
+  .option('-i, --install <path>..[alias]', installDescription, install)
+  .option('-l, --list ', listDescription, list.showList)
+  .option('-d, --delete <alias>', deleteDescription, list.del)
+  .option('-u, --use <alias>..[name]', useDescription, use)
 program
-  .parse(process.argv);
+  .parse(process.argv)
--- test/test.js
+++ test/test.js
@@ -1,43 +1,52 @@
-import test from 'ava';
-const fs = require('fs');
-const exec = require('child_process').execSync;
+import test from 'ava'
+const fs = require('fs')
+const exec = require('child_process').execSync
+
 test.serial('install', (t) => {
-    exec('node f.js -i package.json@test');
-    exec('node f.js -i package.json');
-    t.true(fs.accessSync('./templet/test.json') === undefined);
-    t.true(fs.accessSync('./templet/package.json') === undefined);
-    let list = JSON.parse(fs.readFileSync('./list.json', 'utf8'));
-    t.true(list.test.fileName === 'package.json' && list.test.trueName === 'test.json');
-    t.true(list.package.fileName === 'package.json' && list.package.trueName === 'package.json');
-});
+  exec('node f.js -i package.json@test')
+  exec('node f.js -i package.json')
+  t.true(fs.accessSync('./templet/test.json') === undefined)
+  t.true(fs.accessSync('./templet/package.json') === undefined)
+  let list = JSON.parse(fs.readFileSync('./list.json', 'utf8'))
+  t.true(list.test.fileName === 'package.json' && list.test.trueName === 'test.json')
+  t.true(list.package.fileName === 'package.json' && list.package.trueName === 'package.json')
+  fs.unlinkSync('./templet/test.json')
+  fs.unlinkSync('./templet/package.json')
+})
+
 test.serial('delete', (t) => {
-    exec('node f.js -i package.json@delete');
-    exec('node f.js -d delete');
-    fs.access('./templet/delete.json', (err) => {
-        if (err) t.pass();
-        else t.fail();
-    });
-    let list = JSON.parse(fs.readFileSync('./list.json', 'utf8'));
-    t.true(list.delete === undefined);
-});
+  exec('node f.js -i package.json@delete')
+  exec('node f.js -d delete')
+  fs.access('./templet/delete.json', (err) => {
+    if (err) t.pass()
+    else t.fail()
+  })
+  let list = JSON.parse(fs.readFileSync('./list.json', 'utf8'))
+  t.true(list.delete === undefined)
+})
+
 test.serial('use', (t) => {
-    exec('node f.js -u i');
-    exec('node f.js -u i@initAlias');
-    exec('node f.js -u i@initAlias.js');
-    fs.access('./init.json', (err) => {
-        if (err) t.fail();
-        else t.pass();
-    });
-    fs.access('./initAlias.json', (err) => {
-        if (err) t.fail();
-        else t.pass();
-    });
-    fs.access('./initAlias.js', (err) => {
-        if (err) t.fail();
-        else t.pass();
-    });;
-});
+  exec('node f.js -u i')
+  exec('node f.js -u i@initAlias')
+  exec('node f.js -u i@initAlias.js')
+  fs.access('./init.json', (err) => {
+    if (err) t.fail()
+    else t.pass()
+    fs.unlinkSync('./init.json')
+  })
+  fs.access('./initAlias.json', (err) => {
+    if (err) t.fail()
+    else t.pass()
+    fs.unlinkSync('./initAlias.json')
+  })
+  fs.access('./initAlias.js', (err) => {
+    if (err) t.fail()
+    else t.pass()
+    fs.unlinkSync('./initAlias.js')
+  })
+})
+
 test.serial('list', (t) => {
-    exec('node f.js -l');
-    t.pass();
-});
+  exec('node f.js -l')
+  t.pass()
+})
